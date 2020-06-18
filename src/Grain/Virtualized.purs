dbf7739e81c0c62a421d4f47f605d44a94c323da
Shinya Takahashi <s.takahashi313@gmail.com>
2020-06-19 06:08:02 +0900

    Rename a type synonym

--- src/Grain/Virtualized.purs
+++ src/Grain/Virtualized.purs
@@ -1,5 +1,5 @@
 module Grain.Virtualized
-  ( Config
+  ( Props
   , virtualList
   ) where
 
@@ -13,13 +13,13 @@ import Grain.Markup as H
 import Web.DOM.Element as E
 import Web.Event.Event (target)
 
--- | The type of virtual list config.
+-- | The type of virtual list props.
 -- |
 -- | - `height`: The px height of container
 -- | - `rows`: The data for row view
 -- | - `rowView`: The renderer of a row
 -- | - `calcRowHeight`: The calculator of a row view's px height
-type Config a =
+type Props a =
   { height :: Number
   , rows :: Array a
   , rowView :: { style :: String, row :: a } -> VNode
@@ -40,14 +40,14 @@ instance localGrainScrollTop :: LocalGrain ScrollTop where
   typeRefOf _ = fromConstructor ScrollTop
 
 -- | Render a virtual list.
-virtualList :: forall a. Config a -> VNode
-virtualList config = H.component do
+virtualList :: forall a. Props a -> VNode
+virtualList props = H.component do
   ScrollTop scrollTop <- useValue (LProxy :: _ ScrollTop)
   updateScrollTop <- useUpdater (LProxy :: _ ScrollTop)
 
-  let style = "overflow:auto;height:" <> show config.height <> "px;"
+  let style = "overflow:auto;height:" <> show props.height <> "px;"
 
-      children = calculateChildren config scrollTop
+      children = calculateChildren props scrollTop
 
       onScroll evt =
         case join $ E.fromEventTarget <$> target evt of
@@ -61,51 +61,51 @@ virtualList config = H.component do
     # H.onScroll onScroll
     # H.kids children
 
-calculateChildren :: forall a. Config a -> Number -> Array VNode
-calculateChildren config scrollTop =
-  let params = calculateParams config scrollTop
+calculateChildren :: forall a. Props a -> Number -> Array VNode
+calculateChildren props scrollTop =
+  let params = calculateParams props scrollTop
       topPadding = H.key "_virtualized_top_padding" $ padding params.topPadding
       bottomPadding = H.key "_virtualized_bottom_padding" $ padding params.bottomPadding
    in concat [ [ topPadding ], params.renderingTargets, [ bottomPadding ] ]
 
 calculateParams
   :: forall a
-   . Config a
+   . Props a
   -> Number
   -> Params
-calculateParams config scrollTop =
+calculateParams props scrollTop =
   foldl
-    (calculateParamsByRow config scrollTop)
+    (calculateParamsByRow props scrollTop)
     { topPadding: 0.0
     , bottomPadding: 0.0
     , renderingHeight: 0.0
     , renderingTargets: []
     }
-    config.rows
+    props.rows
 
 calculateParamsByRow
   :: forall a
-   . Config a
+   . Props a
   -> Number
   -> Params
   -> a
   -> Params
-calculateParamsByRow config scrollTop params row =
+calculateParamsByRow props scrollTop params row =
   if params.topPadding + rowHeight < scrollTop then
     params { topPadding = params.topPadding + rowHeight }
-  else if params.renderingHeight < config.height + rowHeight then
+  else if params.renderingHeight < props.height + rowHeight then
     params
       { renderingHeight = params.renderingHeight + rowHeight
       , renderingTargets =
           snoc params.renderingTargets
-            $ calculateRowView config rowHeight row
+            $ calculateRowView props rowHeight row
       }
   else
     params { bottomPadding = params.bottomPadding + rowHeight }
   where
-    rowHeight = config.calcRowHeight row
+    rowHeight = props.calcRowHeight row
 
-calculateRowView :: forall a. Config a -> Number -> a -> VNode
+calculateRowView :: forall a. Props a -> Number -> a -> VNode
 calculateRowView { rowView } rowHeight row =
   let style = "height: " <> show rowHeight <> "px;"
    in rowView { style, row }
